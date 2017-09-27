5c8a5762bf1c4ee0bc16f2c9cff5e68a59bda9cc
Alan Tsai <alan@alantsai.net>
2017-09-27 22:40:44 +0800

    加入一般form post方式

--- src/JQueryPostModelBinding.Web/Controllers/HomeController.cs
+++ src/JQueryPostModelBinding.Web/Controllers/HomeController.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using JQueryPostModelBinding.Web.Models;
 
 namespace JQueryPostModelBinding.Web.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ActionResult Index(FormPostModel model)
         {
-            return View();
+            return View(model);
         }
     }
 }
---
+++ src/JQueryPostModelBinding.Web/Models/FormPostModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JQueryPostModelBinding.Web.Models
+{
+    public class FormPostModel
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
