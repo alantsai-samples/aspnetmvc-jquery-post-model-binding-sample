81db2dac5c0a23969b72519753e32a43ea4fd3ff
Alan Tsai <alan@alantsai.net>
2017-09-29 21:40:05 +0800

    調整物件變成list類型 - 複雜type

--- src/JQueryPostModelBinding.Web/Controllers/HomeController.cs
+++ src/JQueryPostModelBinding.Web/Controllers/HomeController.cs
@@ -10,13 +10,24 @@ namespace JQueryPostModelBinding.Web.Controllers
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index(FormPostModel model)
+        public ActionResult Index(List<FormPostModel> model)
         {
+            if (model == null)
+            {
+                model = new List<FormPostModel>();
+            }
+
+            if (model.Count == 0)
+            {
+                model.Add(new FormPostModel());
+                model.Add(new FormPostModel());
+            }
+
             return View(model);
         }
 
         [HttpPost]
-        public ActionResult JqueryPost(FormPostModel model)
+        public ActionResult JqueryPost(List<FormPostModel> model)
         {
             return PartialView(model);
         }
